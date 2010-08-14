f25767106cbdec1bd9e11d97c39fe111c59ad294
Eire32 <eire32kevin@gmail.com>
2010-08-14 13:19:08 +0100

    Install config errors

--- echelon/inc/config.php
+++ echelon/inc/config.php
@@ -7,40 +7,5 @@ define("DB_CON_ERROR_SHOW", TRUE); // show DB connection error if any (values: T
 define("GRAVATAR", TRUE); // show gravatars image in header (values: TRUE/FALSE)
 define("DB_B3_ERROR_ON", TRUE); // show detailed error messages on B3 DB query failure (values TRUE/FALSE)
 
-#############
-## General ##
-$path = "/echelon/"; // path to echelon from root of web directory. include starting and trailing (eg. "/echelon/" )
-define("PATH", $path);
-
-## Connection info to connect to the database containing the echelon tables
-define("DBL_HOSTNAME", "localhost"); // hostname of where the server is located
-define("DBL_USERNAME", "root"); // username that can connect to that DB
-define("DBL_PASSWORD", ""); // Password for that user
-define("DBL_DB", "echelon"); // name of the database to connect to
-
-#############################
-///// IGNORE BELOW HERE /////
-## Echelon Version ##
-define("ECH_VER", "v.2.0a");
-
-define("SALT", 'mA4CX#qB3O5b'); // do not change ever, this is salt for hashes
-
-$supported_games = array( // supported games
-	"q3a" => "Quake 3 Arena", 
-	"cod" => "Call of Duty", 
-	"cod2" => "Call of Duty 2", 
-	"cod4" => "Call of Duty 4 MW", 
-	"cod5" => "Call of Duty: World at War", 
-	"iourt41" => "Urban Terror", 
-	"wop" => "World of Padman"
-);
-
-// URL to check for updates with
-define("VER_CHECK_URL", "http://echelon.bigbrotherbot.net/version.txt");
-
 // Do not touch this varible
-define("INSTALLED", 'yes');
-
-$ech_log_path = getenv("DOCUMENT_ROOT").PATH."lib/log.txt";
-define("ECH_LOG", $ech_log_path); // location of the Echelon Log file
-unset($ech_log_path);
+define("INSTALLED", 'no');
--- echelon/install/config.php
+++ echelon/install/config.php
@@ -6,4 +6,3 @@ if (!empty($_SERVER["SCRIPT_FILENAME"]) && "config.php" == basename($_SERVER["SC
 define("DB_CON_ERROR_SHOW", TRUE); // show DB connection error if any (values: TRUE/FALSE)
 define("GRAVATAR", TRUE); // show gravatars image in header (values: TRUE/FALSE)
 define("DB_B3_ERROR_ON", TRUE); // show detailed error messages on B3 DB query failure (values TRUE/FALSE)
-
