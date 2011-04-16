e3e60cf4ae61dd8ff7017d7c808dd81c3b29d366
WickedShell <Wicked.Shell.Scripts@gmail.com>
2011-04-15 20:45:27 -0400

    this was part of previous installer overhall, this file is no longer needed

--- echelon/install/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!empty($_SERVER["SCRIPT_FILENAME"]) && "config.php" == basename($_SERVER["SCRIPT_FILENAME"]))
-		die ("Please do not load this page directly. Thanks!"); // do not edit
-
-##### Start Editing From below here #####
-define("DB_CON_ERROR_SHOW", TRUE); // show DB connection error if any (values: TRUE/FALSE)
-define("GRAVATAR", TRUE); // show gravatars image in header (values: TRUE/FALSE)
-define("DB_B3_ERROR_ON", TRUE); // show detailed error messages on B3 DB query failure (values TRUE/FALSE)
